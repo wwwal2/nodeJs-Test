98d230bfcffb1d9f8823a9bd4ed22c1ddbf948b3
wwwal2 <smalex9@gmail.com>
2018-06-29 15:47:15 +0300

    json update

---
+++ data_stash/dataLogic.js
@@ -0,0 +1,13 @@
+// //MAKE JSON
+// var notes = {name: 'Fluffers', age: 2};
+// var jsonRefactor = JSON.stringify(notes);
+//
+// console.log(typeof jsonRefactor);
+// console.log(jsonRefactor);
+//
+// //CONVERT JSON TO JS
+// var jsonStorage = '{"name": "al2", "age": 30}';
+// var javaScriptObject1 = JSON.parse(jsonStorage);
+//
+// console.log(typeof javaScriptObject1);
+// console.log(javaScriptObject1);
